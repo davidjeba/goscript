b7c2cc8f6c7abcce840cfcb7b632c86e6b4f0af0
Founder, CreativeBrandGurus, Consultant & Strategic Mentor <70262709+davidjeba@users.noreply.github.com>
2025-01-29 17:39:55 +0530

    Update component.go

--- pkg/goscript/component.go
+++ pkg/goscript/component.go
@@ -17,7 +17,8 @@ func (f FunctionalComponent) Render() string {
 	return f(nil)
 }
 
-func createElement(component interface{}, props Props, children ...interface{}) string {
+// CreateElement is the public version of our element creation function
+func CreateElement(component interface{}, props Props, children ...interface{}) string {
 	var result strings.Builder
 
 	switch c := component.(type) {
@@ -52,19 +53,5 @@ func createElement(component interface{}, props Props, children ...interface{}) 
 	}
 
 	return result.String()
-}
-
-// Example of a stateful component
-func Counter(props Props) string {
-	count, setCount := UseState("count", 0)
-
-	incrementCount := func() {
-		setCount(count.(int) + 1)
-	}
-
-	return createElement("div", nil,
-		createElement("p", nil, fmt.Sprintf("Count: %d", count)),
-		createElement("button", Props{"onClick": incrementCount}, "Increment"),
-	)
 }
 
